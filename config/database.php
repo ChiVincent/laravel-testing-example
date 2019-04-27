198c3e2534ff1492b347a1e1833912a8924409e7
Vincent Chi <song374561@chivincent.net>
2019-04-27 13:46:48 +0800

    Setup testing database is in-memory sqlite.

--- config/database.php
+++ config/database.php
@@ -42,6 +42,13 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+            'foreign_key_constraints' => true,
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
