7438c316436211ad51d82ff98b244c207a2ad06b
Vincent Chi <song374561@chivincent.net>
2019-04-27 14:08:05 +0800

    Create test case for register.

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -2,16 +2,40 @@
 
 namespace Tests\Feature\Auth;
 
+use App\User;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegisterTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testRegisterPage()
     {
         $response = $this->get(route('register'));
 
         $response->assertSuccessful();
+    }
+
+    public function testRegister()
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->post(route('register'), [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertRedirect(route('home'));
+        $this->assertDatabaseHas('users', [
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
+        $this->assertTrue(
+            Hash::check('password', User::where('email', $user->email)->first()->password)
+        );
     }
 }
